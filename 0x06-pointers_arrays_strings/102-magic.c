37f489e3da4d12e7f394fd840686e300132c1017
BooMujaahid <boomujaahid@gmail.com>
2023-01-02 13:51:09 -0800

    hot fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main(void);
 {
   int n;
   int a[5];
@@ -9,12 +9,12 @@ int main(void)
   a[2] = 1024;
   p = &n;
   /*
-   * write one line of code
-   * Written by BooMujaahid
+   * write your line of code here...
+   * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
-   * - nothing else but this line of code
+   * - you are not allowed to code anything else than this line of code
    */
   *(p + 5) = 98;
   /* ...so that this prints 98\n */
